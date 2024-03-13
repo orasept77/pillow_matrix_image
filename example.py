da32ba14061eb41a16ff6e605332d245d18ea121
Georeperboleja <ishuckn5@gmail.com>
2024-03-13 17:18:01 +0100

    Update example.py

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from path import Path
 from main import gen_qr_code
 
 
-text = "https://skandi-kraft.com/"
-path_to_download = Path().joinpath("example", "LOGO SKANDI KRAFT.png")  # Путь до фона qr кода
+text = "link"
+path_to_download = Path().joinpath("example", "Logo")  # Путь до фона qr кода
 path_to_save = Path().joinpath("example", "example122.png")  # Куда сохранять результат и под каким именем (обязательно в png)
 
 gen_qr_code(text, path_to_download, path_to_save)
